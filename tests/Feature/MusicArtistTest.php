d296a3588e38ceafbb8e53edd299930e2627d103
Renato Álvares <46536452+nato-re@users.noreply.github.com>
2025-09-25 17:19:54 -0300

    Update MusicArtistTest.php

--- tests/Feature/MusicArtistTest.php
+++ tests/Feature/MusicArtistTest.php
@@ -26,11 +26,7 @@ class MusicArtistTest extends TestCase
 
     public function test_artist_and_music_models_have_belongs_to_many_relationships()
     {
-        $album = \App\Models\Album::create([
-            'name' => 'Álbum Teste',
-            'year' => 2020,
-            'url_img' => 'http://img.com/1.png',
-        ]);
+       
         $artist = Artist::create([
             'name' => 'Artista Teste',
             'bio' => 'Bio do artista.'
@@ -38,7 +34,6 @@ class MusicArtistTest extends TestCase
         $music = Music::create([
             'title' => 'Música Teste',
             'artist' => 'Artista Teste',
-            'album_id' => $album->id,
             'year' => 2020,
             'genre' => 'Pop'
         ]);
@@ -49,22 +44,15 @@ class MusicArtistTest extends TestCase
 
     public function test_can_associate_multiple_artists_to_music_and_vice_versa()
     {
-        $album = \App\Models\Album::create([
-            'name' => 'Álbum Teste',
-            'year' => 2020,
-            'url_img' => 'http://img.com/1.png',
-        ]);
         $music1 = Music::create([
             'title' => 'Música 1',
             'artist' => 'Artista 1',
-            'album_id' => $album->id,
             'year' => 2020,
             'genre' => 'Pop'
         ]);
         $music2 = Music::create([
             'title' => 'Música 2',
             'artist' => 'Artista 2',
-            'album_id' => $album->id,
             'year' => 2021,
             'genre' => 'Rock'
         ]);
@@ -76,23 +64,17 @@ class MusicArtistTest extends TestCase
             'name' => 'Artista 2',
             'bio' => 'Bio 2'
         ]);
-    $music1->artists()->attach([$artist1->id, $artist2->id]);
-    $artist2->musics()->attach([$music2->id]); // Só associa o music2, evitando duplicidade
-    $this->assertCount(2, $music1->artists);
-    $this->assertCount(2, $artist2->musics);
+        $music1->artists()->attach([$artist1->id, $artist2->id]);
+        $artist2->musics()->attach([$music2->id]); // Só associa o music2, evitando duplicidade
+        $this->assertCount(2, $music1->artists);
+        $this->assertCount(2, $artist2->musics);
     }
 
     public function test_music_list_displays_artists_names()
     {
-        $album = \App\Models\Album::create([
-            'name' => 'Álbum Teste',
-            'year' => 2020,
-            'url_img' => 'http://img.com/1.png',
-        ]);
         $music = Music::create([
             'title' => 'Música Teste',
             'artist' => 'Artista Teste',
-            'album_id' => $album->id,
             'year' => 2020,
             'genre' => 'Pop'
         ]);
